f05f127c60234682997b6f7f644f54fed9d6b0f3
ghkddlscks19 <ghkddlscks19@naver.com>
2021-09-18 21:47:00 +0900

    20210918

---
+++ src/Recursion/Recursion1901.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Recursion1901 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        num(k);
+    }
+    public static int num(int n){
+        if(n==1){
+            System.out.println(n);
+            return 1;
+        }else {
+            num(n - 1);
+            System.out.println(n);
+            return n;
+        }
+
+    }
+}
